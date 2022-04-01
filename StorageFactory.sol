fe2030b90d9507378fb6dfeb59f85d6265618a83
jurimuller <101435028+jurimuller@users.noreply.github.com>
2022-03-31 20:31:51 -0400

    Update StorageFactory.sol

--- StorageFactory.sol
+++ StorageFactory.sol
@@ -4,13 +4,16 @@ import "./SimpleStorage.sol";
 
 contract StorageFactory is SimpleStorage { //brings in SimpleStorage functions/code
 
-    SimpleStorage[] public simpleStorageArray;
+    //CALL-able: Holds contract addresses
+    SimpleStorage[] public simpleStorageArray; 
 
+    //TRANSACT FN: Populates array 
     function createSimpleStorageContract() public {
-        SimpleStorage simpleStorageObject = new SimpleStorage(); //create object
-        simpleStorageArray.push(simpleStorageObject); //push object to array
+        SimpleStorage simpleStorageObject = new SimpleStorage(); //creates contract/object
+        simpleStorageArray.push(simpleStorageObject); //push contract/object to array
     }
 
+    //TRANSACT FN: Store a number to a contract (specified by index). Errors if no contract at index.
     function sfStore(uint256 _simpleStorageIndex, uint256 _simpleStorageNumber) public {
         SimpleStorage simpleStorageObject = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])); //returns contract to interact w/
         simpleStorageObject.store(_simpleStorageNumber); //interact w/ contract
@@ -19,6 +22,7 @@ contract StorageFactory is SimpleStorage { //brings in SimpleStorage functions/c
         //SimpleStorage(address(simpleStorageArray[_simpleStorageIndex])).store(_simpleStorageNumber); 
     }
 
+    //CALL FN: Return number stored in contract (specified by index)
     function sfGet(uint256 _simpleStorageIndex) public view returns (uint256) {
         SimpleStorage simpleStorageObject = SimpleStorage(address(simpleStorageArray[_simpleStorageIndex]));
         return simpleStorageObject.retrieve();
